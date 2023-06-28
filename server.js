cf6231ea80e22d043cd692118f3503a22c052f17
Patryk Nerć <patryknerc@MacBook-Pro-Patryk.local>
2023-06-28 19:33:48 +0200

    zmiana nazwy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Grupa ZDTESTpol125 jest super SUPER!');
+  res.end('Kto kierowca byl?');
 }
 
 const server = http.createServer(requestListener);
